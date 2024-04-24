7e6f52e0e22293139cef61cd8da4a6935b60dfd6
孙家奇 <777sunzichen@gmail.com>
2024-04-24 15:01:12 +0800

    feat-试一试就试一试

--- api/index.go
+++ api/index.go
@@ -3,8 +3,11 @@ package api
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/pwh-pwh/aiwechat-vercel/db"
 )
 
 func Index(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, "<h1>Hello Aiwechat-Vercel!</h1>")
+	db.ChatDbInstance.SetVideoValue("哈尔滨一九四四", "test")
 }
