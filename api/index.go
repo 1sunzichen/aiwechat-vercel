5ea415668fec1663ed718af72d1629470db98d9e
孙家奇 <777sunzichen@gmail.com>
2024-06-04 16:16:36 +0800

    feat定时器时间

--- api/index.go
+++ api/index.go
@@ -7,13 +7,17 @@ import (
 )
 
 func Index(w http.ResponseWriter, req *http.Request) {
-	ticker := time.NewTicker(1 * time.Second)
+	ticker := time.NewTicker(10 * time.Second)
 	defer ticker.Stop()
 
 	go func() {
 		for t := range ticker.C {
 			s := t.Format("2006-01-02 15:04:05")
-			fmt.Fprintf(w, "<h1>Current time: %s</h1>\n", s)
+			fmt.Fprintf(w, "<h1>Current time2: %s</h1>\n", s)
 		}
 	}()
+	for t := range ticker.C {
+		s := t.Format("2006-01-02 15:04:05")
+		fmt.Fprintf(w, "<h1>Current time1: %s</h1>\n", s)
+	}
 }
