588cbbe0801cd6e8aeba6894e79be93a8a7bec02
孙家奇 <777sunzichen@gmail.com>
2024-06-04 16:12:20 +0800

    feat-更新时间

--- api/index.go
+++ api/index.go
@@ -10,22 +10,10 @@ func Index(w http.ResponseWriter, req *http.Request) {
 	ticker := time.NewTicker(1 * time.Second)
 	defer ticker.Stop()
 
-	done := make(chan bool)
-
 	go func() {
-		for {
-			select {
-			case <-done:
-				return
-			case t := <-ticker.C:
-				s := t.Format("2006-01-02 15:04:05")
-
-				fmt.Fprintf(w, "<h1>Current time"+s+"</h1>")
-			}
+		for t := range ticker.C {
+			s := t.Format("2006-01-02 15:04:05")
+			fmt.Fprintf(w, "<h1>Current time: %s</h1>\n", s)
 		}
 	}()
-
-	time.Sleep(10 * time.Second)
-	done <- true
-	fmt.Fprintf(w, "<h1>Hello Aiwechat-Vercel!</h1>")
 }
