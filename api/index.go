9c90ec5f4f91299f1d8954b18068e8c95c2ff6bc
孙家奇 <777sunzichen@gmail.com>
2024-06-04 16:21:04 +0800

    feat-测试index

--- api/index.go
+++ api/index.go
@@ -7,17 +7,10 @@ import (
 )
 
 func Index(w http.ResponseWriter, req *http.Request) {
-	ticker := time.NewTicker(10 * time.Second)
-	defer ticker.Stop()
+	time.AfterFunc(1*time.Second, func() {
+		fmt.Println("One second later...")
+	})
 
-	go func() {
-		for t := range ticker.C {
-			s := t.Format("2006-01-02 15:04:05")
-			fmt.Fprintf(w, "<h1>Current time2: %s</h1>\n", s)
-		}
-	}()
-	for t := range ticker.C {
-		s := t.Format("2006-01-02 15:04:05")
-		fmt.Fprintf(w, "<h1>Current time1: %s</h1>\n", s)
-	}
+	fmt.Fprintf(w, "Hello, world!")
+
 }
